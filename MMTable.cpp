5fcbbe5dbb89af159cb90711d3ceeeb101b6910a
camillenoufi <camille.noufi@colorado.edu>
2016-04-26 19:17:33 -0600

    Update MMTable.cpp

--- MMTable.cpp
+++ MMTable.cpp
@@ -93,7 +93,7 @@ void MMTable::printTop25Ranking() {
         if (temp -> top25_rank == -1) {
           //
         }else
-          cout << temp -> team << ", "<< temp -> state << ": #" << temp -> top25_rank << endl;
+          cout << "#" <<  temp -> top25_rank << ": "<< temp -> team << ", "<<temp -> state << endl;
         temp = temp -> next;
       }
       bracketExists = 1;
